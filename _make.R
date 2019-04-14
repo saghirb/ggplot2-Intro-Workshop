34a7670e46dd9c071061ebeded062ebf7c358d20
Saghir Bashir <saghir@ilustat.com>
2019-04-14 18:31:50 +0100

    Updated: - Corrected issues in exercises. - Added solutions to exercises.

--- _make.R
+++ _make.R
@@ -8,8 +8,11 @@ here()
 rmarkdown::render(here("Presentation", "ggplot2-Intro.Rmd"),
                   clean = TRUE, output_dir = here("Presentation"))
 
-rmarkdown::render(here("Exercises", "ggplot2-exercises.Rmd"),
-                   clean = TRUE, output_dir = here("Exercises"))
+rmarkdown::render(here("Exercises", "ggplot2-Exercises.Rmd"),
+                  clean = TRUE, output_dir = here("Exercises"))
+
+rmarkdown::render(here("Exercises", "ggplot2-Solutions.Rmd"),
+                  clean = TRUE, output_dir = here("Exercises"))
 
 # Create a PDF version of the slides to share
 library(webshot)
@@ -33,7 +36,7 @@ file.copy(here("Presentation", "ggplot2-Intro.html"),
 file.copy(here("Presentation", "ggplot2-Intro.pdf"),
           here("Share", "Slides-Notes"), overwrite = TRUE)
 
-file.copy(here("Exercises", "ggplot2-exercises.Rmd"),
+file.copy(here("Exercises", "ggplot2-Exercises.Rmd"),
           here("Share", "Exercises"), overwrite = TRUE)
 
 file.copy(here("Exercises", "data", "gapminder.csv"),
@@ -42,11 +45,14 @@ file.copy(here("Exercises", "data", "gapminder.csv"),
 # Creating (initialising) an RStudio project
 rstudioapi::initializeProject(path = here("Share", "Exercises"))
 file.rename(here("Share", "Exercises", "Exercises.Rproj"),
-            here("Share", "Exercises", "ggplot2-exercises.Rproj"))
+            here("Share", "Exercises", "ggplot2-Exercises.Rproj"))
 
 # Using here() function with zip results in full paths in the zip files :(
 # Not beautiful: Using setwd to overcome the full paths issue above.
 setwd(here("Share"))
 zip(here("Share", "ggplot2-Intro.zip"), ".", extras = "-FS")
+setwd(here("Exercises"))
+zip(here("Share", "ggplot2-Solutions.zip"),
+    c("ggplot2-Solutions.Rmd", "ggplot2-Solutions.html"), extras = "-FS")
 setwd(here())
 
